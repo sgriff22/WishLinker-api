cd1421510bbc726a73270246ecb27a11a7c93066
Stacy Griffy <sgriffy2@gmail.com>
2024-05-01 14:22:23 -0500

    created a Friends viewset with an update method to mark accepted true

--- wishapi/views/__init__.py
+++ wishapi/views/__init__.py
@@ -2,3 +2,4 @@ from .users import UserViewSet, UserSerializer
 from .wishlists import WishlistViewSet, WishlistSerializer
 from .priorities import PriorityViewSet
 from .profile import ProfileViewSet
+from .friends import FriendViewSet
---
+++ wishapi/views/friends.py
@@ -0,0 +1,43 @@
+from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework import status
+from wishapi.models import Friend
+
+
+class FriendViewSet(viewsets.ViewSet):
+    """View for interacting with user friends"""
+
+    def update(self, request, pk=None):
+        """
+        @api {PUT} /friends/:id PUT accepted value for friends
+        @apiName UpdateFriend
+        @apiGroup Friends
+
+        @apiHeader {String} Authorization Auth token
+        @apiHeaderExample {String} Authorization
+            Token 9ba45f09651c5b0c404f37a2d2572c026c146611
+
+        @apiParam {Number} id Friend ID (route parameter)
+        @apiParam {Boolean} accepted Accepted status for the friend request
+        @apiParamExample {json} Input
+            {
+                "accepted": true
+            }
+
+        @apiSuccessExample {json} Success
+            HTTP/1.1 204 No Content
+        """
+
+        accepted_value = request.data.get("accepted")
+
+        try:
+            friend = Friend.objects.get(pk=pk)
+        except Friend.DoesNotExist:
+            return Response(
+                "Friend instance not found", status=status.HTTP_404_NOT_FOUND
+            )
+
+        friend.accepted = accepted_value
+        friend.save()
+
+        return Response({}, status=status.HTTP_204_NO_CONTENT)
