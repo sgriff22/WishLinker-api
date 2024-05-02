74399b94bc7ed8fc76fb3bfd155b4567b48b00c8
Stacy Griffy <sgriffy2@gmail.com>
2024-05-02 11:41:46 -0500

    Created get all users method to get all the users except the current user and his/her friends. Included search filtering

--- wishapi/views/friends.py
+++ wishapi/views/friends.py
@@ -2,6 +2,10 @@ from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework import status
 from wishapi.models import Friend
+from django.contrib.auth.models import User
+from wishapi.views import UserSerializer
+from django.db.models import Q
+from rest_framework.decorators import action
 
 
 class FriendViewSet(viewsets.ViewSet):
@@ -63,3 +67,36 @@ class FriendViewSet(viewsets.ViewSet):
         friend.delete()
 
         return Response({}, status=status.HTTP_204_NO_CONTENT)
+
+    @action(detail=False, methods=["get"])
+    def get_all_users(self, request):
+        """
+        Get all users in the database, excluding the current user and the user's friends.
+        """
+        current_user = request.user
+
+        # Retrieve the IDs of the user's friends
+        friend_ids = Friend.objects.filter(
+            Q(user1=current_user, accepted=True) | Q(user2=current_user, accepted=True)
+        ).values_list("user1_id", "user2_id")
+
+        # Extract all friend IDs into a single list
+        all_friend_ids = [
+            friend_id for friend_pair in friend_ids for friend_id in friend_pair
+        ]
+
+        # Exclude the current user and the user's friends from the query
+        users = User.objects.exclude(Q(id=current_user.id) | Q(id__in=all_friend_ids))
+
+        # Filter friends by name if search query is provided
+        search_query = request.query_params.get("q", None)
+        if search_query:
+            users = users.filter(
+                Q(first_name__icontains=search_query)
+                | Q(last_name__icontains=search_query)
+                | Q(username__icontains=search_query)
+            )
+
+        # Serialize the users and return the response
+        serializer = UserSerializer(users, many=True)
+        return Response(serializer.data)
